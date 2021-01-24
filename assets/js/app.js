d1442c7095cdf386215f31d32d973e62f5f61b7f
Sardor <asrorjon5538@gmail.com>
2021-01-24 22:39:24 +0500

    file input addedside-

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,20 @@ $(document).ready(function () {
         $('.overlay').toggleClass('opened')
 
     })
+    $('.side-bar-btn').click(function(){
+        $('.side-bar').toggleClass('opened')
+    })
+    $('.lock-btn').click(function () {
+        $('.lock-icon').toggleClass('fa-lock-open');
+        $('.lock-screen').toggleClass('locked-screen');
+        $('.lock-icon').toggleClass('fa-lock');
+    })
+    $('.psw-input').focus(function() {
+        $('.psw-label').toggleClass('go-up')
+    })
+    $('.psw-form').submit(function(){
+                $('.lock-screen').removeClass('locked-screen');
+        $('.lock-icon').addClass('fa-lock-open');
+        $('.lock-icon').removeClass('fa-lock');
+    })
 })
